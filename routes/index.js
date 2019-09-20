dcdc3d8b0b8945181b1341e28d7e122c7cc42265
Karolina Soos <karosoos@Karolinas-MacBook-Pro.local>
2019-09-19 19:32:50 -0700

    adding page 1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Karolinas Page' });
+});
+
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Page 2' });
+});
+
+router.get('/page3', function(req, res, next) {
+  res.render('page3', { title: 'Page 3' });
+});
+
 module.exports = router;
