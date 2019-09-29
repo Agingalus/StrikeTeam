c1fc215789def16017376ce716f9364baee91d47
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-09-28 17:50:14 -0700

    Added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SimpletextComponent } from './simpletext/simpletext.component';
 import { CalculateComponent } from './calculate/calculate.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { Calculate2Component } from './calculate2/calculate2.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpletextComponent,
-    CalculateComponent
+    CalculateComponent,
+    Calculate2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculate2/calculate2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate2',
+  templateUrl: './calculate2.component.html',
+  styleUrls: ['./calculate2.component.css']
+})
+export class Calculate2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
